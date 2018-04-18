e80dd0e54ed1aef445e6b23f03e70d1f103bcc74
Fabian Schuh <Fabian@chainsquad.com>
2018-04-18 15:28:31 +0200

    setup and license

--- setup.py
+++ setup.py
@@ -2,27 +2,30 @@
 
 from setuptools import setup
 import sys
+
 from bos_incidents import __VERSION__
 
 assert sys.version_info[0] == 3, "We require Python > 3"
 
 setup(
-    name='peerplays_bos_incidents',
+    name='bos-incidents',
     version=__VERSION__,
     description=(
         'Private module for BOS incident storage'
     ),
     long_description=open('README.md').read(),
+    download_url='https://github.com/pbsa/bos-incidents/tarball/' + __VERSION__,
     author='Blockchain BV',
     author_email='info@blockchainbv.com',
     maintainer='Stefan Schießl',
     maintainer_email='Stefan.Schiessl@BlockchainProjectsBV.com',
+    url='http://pbsa.info',
     keywords=['peerplays', 'bos'],
     packages=[
         "bos_incidents",
     ],
     classifiers=[
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Development Status :: 3 - Alpha',
